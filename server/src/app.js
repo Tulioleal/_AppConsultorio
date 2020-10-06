7ecefcb37f9b2270f26b4e2660a4f474c9c1fd1a
Tulio <tulioleal13@gmail.com>
2020-10-06 17:42:17 -0300

    add cors module

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express()
 
 //variables de entorno
 app.set('port', process.env.PORT || 4000)
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))

f8894275ba2f4a038b1b88df7e9b4949c3f95bcd
Tulio <tulioleal13@gmail.com>
2020-10-07 20:56:44 -0300

    import form modules

--- AppConsultorio/src/app/app.module.ts
+++ AppConsultorio/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { EditarPacienteComponent } from './components/editar-paciente/editar-pac
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

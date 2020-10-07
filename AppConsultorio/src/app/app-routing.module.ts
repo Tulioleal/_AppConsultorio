93c5c1c440ee221114b3852615f0b48e156a68a1
Tulio <tulioleal13@gmail.com>
2020-10-07 12:59:19 -0300

    creating routes

--- AppConsultorio/src/app/app-routing.module.ts
+++ AppConsultorio/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//components
+import { HomeComponent } from './components/home/home.component'
+import { PacienteComponent } from './components/paciente/paciente.component'
+import { FormularioPacienteComponent } from './components/formulario-paciente/formulario-paciente.component'
+import { FormularioCitaComponent } from './components/formulario-cita/formulario-cita.component'
+import { EditarPacienteComponent } from './components/editar-paciente/editar-paciente.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"pacientes/addCita", component: FormularioCitaComponent },
+  { path:"pacientes/editar", component: EditarPacienteComponent },
+  { path:"pacientes/addPaciente", component: FormularioPacienteComponent },
+  { path:"pacientes", component: PacienteComponent },
+  { path:"home", component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AppConsultorio/src/app/app.module.ts
+++ AppConsultorio/src/app/app.module.ts
@@ -5,11 +5,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PacienteComponent } from './components/paciente/paciente.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormularioPacienteComponent } from './components/formulario-paciente/formulario-paciente.component';
+import { FormularioCitaComponent } from './components/formulario-cita/formulario-cita.component';
+import { EditarPacienteComponent } from './components/editar-paciente/editar-paciente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PacienteComponent
+    PacienteComponent,
+    HomeComponent,
+    FormularioPacienteComponent,
+    FormularioCitaComponent,
+    EditarPacienteComponent
   ],
   imports: [
     BrowserModule,

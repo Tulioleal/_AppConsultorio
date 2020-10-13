df356ba171e51d4dfbb823c8e841c978624b9277
Tulio <tulioleal13@gmail.com>
2020-10-13 16:29:07 -0300

    Navigation component was created

---
+++ AppConsultorio/src/app/components/navegacion/navegacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navegacion',
+  templateUrl: './navegacion.component.html',
+  styleUrls: ['./navegacion.component.scss']
+})
+export class NavegacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

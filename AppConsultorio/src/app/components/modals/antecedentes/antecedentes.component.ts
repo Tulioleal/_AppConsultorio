195f493bf59f987c7d90298c8a7d6ab852f3a6be
Tulio <tulioleal13@gmail.com>
2020-10-14 19:45:36 -0300

    modal components added

---
+++ AppConsultorio/src/app/components/modals/antecedentes/antecedentes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-antecedentes',
+  templateUrl: './antecedentes.component.html',
+  styleUrls: ['./antecedentes.component.scss']
+})
+export class AntecedentesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

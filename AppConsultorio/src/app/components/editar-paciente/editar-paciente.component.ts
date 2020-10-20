4b6abc89a30db54a3bcbefd8f829239433363912
Tulio <tulioleal13@gmail.com>
2020-10-20 19:24:56 -0300

    creating editarComponent(style, structure & logic)

--- AppConsultorio/src/app/components/editar-paciente/editar-paciente.component.ts
+++ AppConsultorio/src/app/components/editar-paciente/editar-paciente.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { from } from 'rxjs';
+import { PacienteService } from 'src/app/services/paciente.service';
 
 @Component({
   selector: 'app-editar-paciente',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPacienteComponent implements OnInit {
 
-  constructor() { }
+  constructor( public pacienteService: PacienteService, private snackbar: MatSnackBar ) { }
 
   ngOnInit(): void {
   }
 
+  openSnackBar() {
+    this.snackbar.open('Paciente Editada exitosamente', `Cerrar`, {
+      duration: 4000
+    })
+  }
+
+  cancelEdition(form:NgForm){
+    if  ( confirm("You will lose the new information in this form") ) {
+      this.resetForm(form)
+    }
+  }
+
+  resetForm(form:NgForm){
+    form.reset()
+  }
+
+  updatePaciente(form: NgForm){
+    this.pacienteService.updatePaciente(form.value).subscribe(
+      res => {
+        console.log(res)
+        this.openSnackBar()
+      },
+      err => console.log(err)
+    )
+  }
 }

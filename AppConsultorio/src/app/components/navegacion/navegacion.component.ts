edae3dfd8eedfc8e5537df17c56a88fb8a06570a
Tulio <tulioleal13@gmail.com>
2020-10-20 19:43:24 -0300

    adding clearForm

--- AppConsultorio/src/app/components/navegacion/navegacion.component.ts
+++ AppConsultorio/src/app/components/navegacion/navegacion.component.ts
@@ -1,4 +1,9 @@
+//Angular
 import { Component, OnInit } from '@angular/core';
+
+//Services
+import { PacienteService } from '../../services/paciente.service'
+import { Paciente } from 'src/app/models/paciente';
 
 @Component({
   selector: 'app-navegacion',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavegacionComponent implements OnInit {
 
-  constructor() { }
+  constructor( public pacienteService: PacienteService ) { }
 
   ngOnInit(): void {
+  }
+
+  clearForm(){
+    this.pacienteService.selectedPaciente = {
+      nombre: "",
+      apellido: "",
+      nacimiento: "",
+      telf: "",
+      email: "",
+      seguimiento: false,
+      antecedentePersonal: "",
+      antecedenteFamiliar: "",
+      ginecoMenorrea: false,
+      ginecoGestas: 0,
+      ginecoParas: 0,
+      ginecoAbortos: 0,
+      ginecoCesareas: 0,
+      ginecoOtros: 0,
+      ginecoMetodo: "",
+      ginecoMenopausia : 0,
+      ginecoTRH: "",
+    }
   }
 
 }

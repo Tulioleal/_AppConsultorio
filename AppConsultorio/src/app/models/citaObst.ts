ed2bb398c50ca717f5cf0f2f85c08d0a691d96fa
Tulio <tulioleal13@gmail.com>
2020-11-06 17:47:36 -0300

    creating CitaObst Service and model

---
+++ AppConsultorio/src/app/models/citaObst.ts
@@ -0,0 +1,34 @@
+export interface CitaObst {
+
+  pacienteId: string
+  visita: number
+  motivo: string
+  HistEnfEvo: string
+  HistClinPre: string
+  ultMenst: string
+  penMenst: string
+  fechaEmb: string
+  semanas: number
+  diasEmb: number
+  meses: number
+  conDias: number
+  pesoAEmb: number
+  altura: number
+  percepFetal: string
+  exGenEstadoG: string
+  exGenPA: string
+  exGenPulso: string
+  exGenFR: string
+  exGenPeso: number
+  exGenAlt: number
+  exObsAbdom: string
+  exObsMovFet: string
+  exObsAltUt: string
+  exObsCircunAbdom: string
+  exObsFCF: string
+  exGineco: string
+  createdAt?: string
+  updatedAt?: string
+  _id?: string
+
+}
--- AppConsultorio/src/app/services/cita-obst.service.ts
+++ AppConsultorio/src/app/services/cita-obst.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { CitaObst } from '../models/citaObst'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitaObstService {
 
-  constructor() { }
+  URI_API = 'http://localhost:4000/api/citasObst'
+
+  selectedCitaObst : CitaObst = {
+    pacienteId: "",
+    visita: 1,
+    motivo: "",
+    HistEnfEvo: "",
+    HistClinPre: "",
+    ultMenst: "",
+    penMenst: "",
+    fechaEmb: "",
+    semanas: 1,
+    diasEmb: 1,
+    meses: 1,
+    conDias: 1,
+    pesoAEmb: 1,
+    altura: 1,
+    percepFetal: "",
+    exGenEstadoG: "",
+    exGenPA: "",
+    exGenPulso: "",
+    exGenFR: "",
+    exGenPeso: 1,
+    exGenAlt: 1,
+    exObsAbdom: "",
+    exObsMovFet: "",
+    exObsAltUt: "",
+    exObsCircunAbdom: "",
+    exObsFCF: "",
+    exGineco: ""
+  }
+
+  citasObst : CitaObst[]
+  citaObst : CitaObst
+
+  constructor( private http: HttpClient ) { }
+
+  getCitasObst(){
+    return this.http.get<CitaObst>(`${this.URI_API}`)
+  }
+
+  getCitaObst(_id : string){
+    return this.http.get<CitaObst>(`${this.URI_API}/${_id}`)
+  }
+
+  createCitaObst( citaObst : CitaObst ){
+    return this.http.post(this.URI_API, citaObst)
+  }
+
+  updateCitaObst( citaObst : CitaObst ){
+    return this.http.put(`${this.URI_API}/${citaObst._id}`, citaObst)
+  }
+
+  deleteCitaObst( _id: string ){
+    return this.http.delete(`${this.URI_API}/${_id}`)
+  }
+
 }

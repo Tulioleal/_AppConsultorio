f8c45480e94452c54ca0b667fd0e9a11d30da56b
Tulio <tulioleal13@gmail.com>
2020-11-06 15:19:21 -0300

    add citaGineco Model

---
+++ AppConsultorio/src/app/models/citaGineco.ts
@@ -0,0 +1,18 @@
+export interface CitaGineco {
+  pacienteId: string
+  visita: Number
+  motivo: string
+  HistEnfEvo: string
+  genEstado: string
+  genComentarios: string
+  ginecoMamas: string
+  ginecoGen: string
+  ginecoGenExt: string
+  ginecoCervix: string
+  ginecoUtero: string
+  ginecoImpClin: string
+  ginecoColpos: string
+  createdAt?: string
+  updatedAt?: string
+  _id?: string
+}

93fef2b8450c559b0458a6d3f9301ad3a59c35d6
Tulio <tulioleal13@gmail.com>
2020-11-09 16:01:21 -0300

    structure & functionality to add-cita-gineco comp

--- AppConsultorio/src/app/components/cita/add-cita-gineco/add-cita-gineco.component.ts
+++ AppConsultorio/src/app/components/cita/add-cita-gineco/add-cita-gineco.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms'
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+//SERIVICE
+import { CitaGinecoService } from '../../../services/cita-gineco.service'
 
 @Component({
   selector: 'app-add-cita-gineco',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCitaGinecoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public citaGinecoService : CitaGinecoService,
+    private snackbar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
   }
 
+  addCitaGineco( form : NgForm ){
+    this.citaGinecoService.createCitaGineco(form.value).subscribe(
+      res => {
+        console.log(res)
+        this.openSnackBar()
+      },
+      err => {
+        console.log(err)
+      }
+    )
+  }
+
+  openSnackBar() {
+    this.snackbar.open('Cita Cita Created', 'Cerrar', {
+      duration: 2000
+    })
+  }
 }

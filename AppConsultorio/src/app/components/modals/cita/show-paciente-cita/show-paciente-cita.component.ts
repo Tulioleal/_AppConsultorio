c044924832e66c621eb034c8ff8dd57baa463ca4
Tulio <tulioleal13@gmail.com>
2020-11-20 17:24:12 -0300

    add style, getCita and OpenSpecific cita

--- AppConsultorio/src/app/components/modals/cita/show-paciente-cita/show-paciente-cita.component.ts
+++ AppConsultorio/src/app/components/modals/cita/show-paciente-cita/show-paciente-cita.component.ts
@@ -1,44 +1,87 @@
+//Angular
 import { Component, OnInit } from '@angular/core';
-import { PacienteService } from 'src/app/services/paciente.service';
 
-import { CitaGinecoService } from '../../../../services/cita-gineco.service'
-import { CitaObstService } from '../../../../services/cita-obst.service'
+//Material
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+
+//Components
+import { ShowSpecificCitaComponent } from '../show-specific-cita/show-specific-cita.component';
+
+//Services
+import { PacienteService } from 'src/app/services/paciente.service';
+import { CitaGinecoService } from '../../../../services/cita-gineco.service';
+import { CitaObstService } from '../../../../services/cita-obst.service';
 
 @Component({
   selector: 'app-show-paciente-cita',
   templateUrl: './show-paciente-cita.component.html',
-  styleUrls: ['./show-paciente-cita.component.scss']
+  styleUrls: ['./show-paciente-cita.component.scss'],
 })
 export class ShowPacienteCitaComponent implements OnInit {
-
   constructor(
-
-    public pacienteService : PacienteService,
-    public ginecoService : CitaGinecoService,
-    public obstService : CitaObstService,
-
-  ) { }
+    public pacienteService: PacienteService,
+    public ginecoService: CitaGinecoService,
+    public obstService: CitaObstService,
+    public dialog: MatDialog,
+  ) {}
 
   ngOnInit(): void {
-    this.getCitas( this.pacienteId )
+    this.getCitas(this.pacienteId);
   }
 
-  pacienteId : string = this.pacienteService.selectedPaciente._id
+  numeroG: number;
+  numeroO: number;
 
-  getCitas(
-    pacienteId: string = this.pacienteId
-  ){
+  pacienteId: string = this.pacienteService.selectedPaciente._id;
+
+  getCitas(pacienteId: string = this.pacienteId) {
     this.ginecoService.getCitasGineco(pacienteId).subscribe(
-      res =>{
-        this.ginecoService.citasGineco = res
+      (res) => {
+        this.ginecoService.citasGineco = res;
+        this.numeroG = this.ginecoService.citasGineco.length;
       },
-      err => { console.log(err)}
-    )
+      (err) => console.log(err)
+    );
     this.obstService.getCitasObst(pacienteId).subscribe(
-      res => {
-        this.obstService.citasObst = res
+      (res) => {
+        this.obstService.citasObst = res;
+        this.numeroO = this.obstService.citasObst.length;
       },
-      err => console.log(err)
-    )
+      (err) => console.log(err)
+    );
+  }
+
+  getCita(pacienteId: string, id : string, gestas?: number){
+    if(gestas){
+      this.obstService.getCitaObst(pacienteId, gestas, id).subscribe(
+        res =>{
+          this.obstService.selectedCitaObst = res
+        },
+        err => console.log(err)
+      )
+    }else{
+      this.ginecoService.getCitaGineco(pacienteId, id).subscribe(
+        res =>{
+          this.ginecoService.selectedCitaGineco = res
+        },
+        err => console.log(err)
+      )
+    }
+  }
+
+  openSpecificCita(id: string, type: string) {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = false;
+    dialogConfig.data = { id: id, type: type };
+
+    if (type === 'gineco') {
+      dialogConfig.width = '50%';
+      dialogConfig.height = '50%';
+    } else {
+      dialogConfig.width = '60%';
+      dialogConfig.height = '60%';
+    }
+
+    this.dialog.open(ShowSpecificCitaComponent, dialogConfig);
   }
 }

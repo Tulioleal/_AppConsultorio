ab3834e207d57667716f8db9575dd4245fc47825
Tulio <tulioleal13@gmail.com>
2020-11-20 17:22:38 -0300

    add edit cita Gineco and Obst

---
+++ AppConsultorio/src/app/components/modals/cita/edit/edit-cita-obst/edit-cita-obst.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cita-obst',
+  templateUrl: './edit-cita-obst.component.html',
+  styleUrls: ['./edit-cita-obst.component.scss']
+})
+export class EditCitaObstComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

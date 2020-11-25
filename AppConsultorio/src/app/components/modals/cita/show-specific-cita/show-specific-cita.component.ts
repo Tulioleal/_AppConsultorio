947e56262ecda487a6791d5d7e954f9e2ed51a3e
Tulio <tulioleal13@gmail.com>
2020-11-25 19:23:51 -0300

    add style, structure & logic to specific-cita

--- AppConsultorio/src/app/components/modals/cita/show-specific-cita/show-specific-cita.component.ts
+++ AppConsultorio/src/app/components/modals/cita/show-specific-cita/show-specific-cita.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CitaGinecoService } from 'src/app/services/cita-gineco.service';
 import { CitaObstService } from 'src/app/services/cita-obst.service';
@@ -10,6 +10,13 @@ import { CitaObstService } from 'src/app/services/cita-obst.service';
 })
 export class ShowSpecificCitaComponent implements OnInit {
 
+  meses: number;
+  semanas: number;
+  conDias: number;
+  semDias: number;
+  dias: number;
+  fechaProbable : Date
+
   constructor(
     public ginecoService : CitaGinecoService,
     public obstService : CitaObstService,
@@ -17,6 +24,25 @@ export class ShowSpecificCitaComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.fProbable()
+    this.compararFechas()
   }
+
+  compararFechas(){
+    let fechaTime = Date.parse(this.obstService.selectedCitaObst.ultMenst)
+    let dif : number = this.obstService.date - fechaTime
+
+    this.meses = Math.floor(dif / 2629800000)
+    this.conDias = Math.floor((dif % 2629800000) / 86400000 )
+    this.semanas = Math.floor(dif / 604800000 )
+    this.semDias = Math.floor((dif % 604800000 ) / 86400000 )
+    this.dias = Math.floor(dif / 86400000 )
+  }
+
+  fProbable(){
+    this.obstService.fechaProbable()
+    this.fechaProbable = this.obstService.fProbable
+  }
+
 
 }

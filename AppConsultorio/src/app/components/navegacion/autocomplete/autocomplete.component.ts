3c2312e0628965ed1070f1f439deee378557ac57
Tulio <tulioleal13@gmail.com>
2020-11-27 20:09:22 -0300

    fix bugs

--- AppConsultorio/src/app/components/navegacion/autocomplete/autocomplete.component.ts
+++ AppConsultorio/src/app/components/navegacion/autocomplete/autocomplete.component.ts
@@ -1,9 +1,8 @@
 //Angular
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subscription, Observable } from 'rxjs';
 import { FormControl } from '@angular/forms';
 import { map, startWith } from 'rxjs/operators';
-import { HttpErrorResponse } from '@angular/common/http';
 
 //Components
 import { PacienteService } from '../../../services/paciente.service';

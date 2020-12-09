17fe7140e768607fa025cef3242d16c0229ca3f4
Tulio <tulioleal13@gmail.com>
2020-12-09 12:54:35 -0300

    prod

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path')
 
 const app = express()
+const PORT = process.env.PORT || 5000
+let appPath = '../../AppConsultorio/dist/AppConsultorio/'
 
 //variables de entorno
 app.set('port', process.env.PORT || 4000)
@@ -18,5 +21,10 @@ app.use('/api/pacientes', require('./routes/pacientes.routes'))
 app.use('/api/citasGineco', require('./routes/citaGineco.routes'))
 //CitasObst
 app.use('/api/citasObst', require('./routes/citaObst.routes'))
+
+//Listen App
+app.use('/',  express.static(path.join(__dirname, appPath)))
+.get('*', (req,res)=> res.sendFile(path.join(__dirname, appPath + 'index.html')))
+.listen(PORT)
 
 module.exports = app
--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,7 @@
 require('dotenv').config();
-const express = require('express')
-
-const exp = express()
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-let appPath = '../../AppConsultorio/dist/AppConsultorio/'
-
-const app = require('./app');
 require('./database');
 
-exp.use('/',  express.static(path.join(__dirname, appPath)))
-.get('*', (eq,res)=> res.sendFile(path.join(__dirname, appPath + 'index.html')))
-.listen(PORT, ()=> console.log(`Listening on ${PORT}`))
+const app = require('./app');
 
 app.listen(app.get('port'), () => {
 

50b7a69a78a675c961cf2cfbb705bce0339c85e0
Tulio <tulioleal13@gmail.com>
2021-01-04 17:44:43 -0300

    improve UX

--- src/app/components/auth/sign-in/sign-in.component.ts
+++ src/app/components/auth/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -12,23 +13,33 @@ export class SignInComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
+    private snackbar: MatSnackBar,
     private router: Router,
 
   ) { }
 
   ngOnInit(): void {
+    this.authService.clearForms()
   }
 
   signInUser(form: NgForm){
     this.authService.signIn(form.value).subscribe(
       res => {
         localStorage.setItem('refreshToken', res.refreshToken)
+        this.openSnackBar(`Welcome`)
         this.router.navigate(['/pacientes'])
       },
       err => {
         console.log(err)
+        this.openSnackBar('Email or Password invalid')
       }
     )
+  }
+
+  private openSnackBar(message:string) {
+    this.snackbar.open( message , 'Cerrar' , {
+      duration: 2000
+    })
   }
 
 }
--- src/app/components/auth/sign-up/sign-up.component.ts
+++ src/app/components/auth/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -12,6 +13,7 @@ export class SignUpComponent implements OnInit {
 
   constructor(
     public authService: AuthService,
+    private snackbar: MatSnackBar,
     private router: Router
   ) { }
 
@@ -23,25 +25,43 @@ export class SignUpComponent implements OnInit {
   }
 
   signUpUser( form: NgForm ){
-    this.authService.signUp(form.value).subscribe(
-      res => {
-        console.log('signup succesful')
-        this.router.navigate(['auth/signin'])
-      },
-      err => {
-        console.log(err)
-      }
-    )
+
+    if ( this.compararPassword() ) {
+
+      this.authService.signUp(form.value).subscribe(
+        res => {
+          this.openSnackBar('User successfully created')
+          this.router.navigate(['auth/signin'])
+        },
+        err => {
+          console.log(err)
+          this.openSnackBar('Email or Username is already in use')
+        }
+      )
+      return
+
+    } else {
+      this.openSnackBar('The password do not match')
+      return
+    }
   }
 
-  compararPassword(){
+  private compararPassword(){
 
     this.password1 = this.authService.selectedUserSignUp.password
-
     let comparacion: boolean = this.password1 == this.password2
 
-    if ( comparacion ) return this.isValid = true
-    else return this.isValid = false
-
+    if(comparacion) {
+      return true
+    }else{
+      return false
+    }
   }
+
+  private openSnackBar(message:string) {
+    this.snackbar.open( message , 'Cerrar' , {
+      duration: 2000
+    })
+  }
+
 }
